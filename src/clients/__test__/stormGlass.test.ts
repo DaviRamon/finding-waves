6880beb3b3662f89e5a62a61d60c61bd23eda813
Davi Ramon <davi.goncalves@solarview.com.br>
2022-09-12 19:20:51 -0300

    adicionando dados para testes da req e dados normalizados para desenvolvimento

--- src/clients/__test__/stormGlass.test.ts
+++ src/clients/__test__/stormGlass.test.ts
@@ -1,5 +1,6 @@
 import { StormGlass } from "@src/clients/stormGlass";
 import axios from 'axios';
+import stormGlassWeather3HoursFixture from '@test/fixtures/stormglass_weather_3_hours.json'; // importa o JSON com a resposta da requisição para testes
 
 jest.mock('axios');
 
@@ -8,7 +9,7 @@ describe('StormGlass client', () => {
           const lat = -26.1174;
           const lng = -48.6168;
 
-          axios.get = jest.fn().mockResolvedValue({});
+          axios.get = jest.fn().mockResolvedValue(stormGlassWeather3HoursFixture);
 
           const stormGlass = new StormGlass(axios);
           const response = await stormGlass.fetchPoints(lat, lng);
--- src/clients/stormGlass.ts
+++ src/clients/stormGlass.ts
@@ -1,13 +1,14 @@
 import { AxiosStatic } from "axios";
 
 export class StormGlass {
-     
+
+     readonly stormGlassAPIParams = 'swellDirection,swellHeight,swellPeriod,waveDirection,waveHeight,windDirection,windSpeed';
+     readonly stormGlassAPISource ='noaa'
      constructor(protected request: AxiosStatic){}
 
      public async fetchPoints(lat:number, lng:number):Promise<{}> {
-          let url = `https://api.stormglass.io/v2/weather/point?params=swellDirection%2CswellHeight%2CswellPeriod%2CwaveDirection%2CwaveHeight%2CwindDirection%2CwindSpeed&source=noaa&lat=${lat}&lng=${lng}`
+          let url = `https://api.stormglass.io/v2/weather/point?params=${this.stormGlassAPIParams}&source=${this.stormGlassAPISource}&lat=${lat}&lng=${lng}`
           return this.request.get(url);
- 
      }
 
 }
