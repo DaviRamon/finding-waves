16b51cbe8fa2e03afa97e83fe92e7888c5015cfb
Davi Ramon <davi.goncalves@solarview.com.br>
2022-09-12 16:37:27 -0300

    MASTER: finalinzado testes

---
+++ test/jest-setup.ts
@@ -0,0 +1,10 @@
+// arquivo responsavel para iniciar os servidores para os testes funcionais.
+
+import { SetupServer } from "@src/server";
+import supertest from "supertest";
+
+beforeAll(() => {
+     const server = new SetupServer();
+     server.init();
+     global.testRequest = supertest(server.getApp());
+});
